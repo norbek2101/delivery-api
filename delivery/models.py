1e281cbddb5fa8e01e7465d9d66d48505f690576
norbek <lightnight1223@gmail.com>
2021-10-29 22:00:01 +0500

    Branch, Product, Order models was created

--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Branch(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Branch'
+        verbose_name_plural = 'Branches'  
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Order(models.Model):
+    branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    phone = models.PositiveIntegerField()
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    address = models.CharField(max_length=200)
+    definition = models.TextField(blank=True, null=True)
+    time = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    STATUS = [
+        ('in order', 'in order'),
+        ('confirmed', 'confirmed'),
+        ('food completed', 'food completed'),
+        ('in way', 'in way'),
+        ('order completed', 'order completed')
+    ]
+    status = models.CharField(max_length=200, choices=(STATUS), default='in order')
+
+    def __str__(self):
+        return self.name
+
