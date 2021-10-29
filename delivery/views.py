35b39da540fdfbbbb3825f47b7dfba1e470b9a8c
norbek <lightnight1223@gmail.com>
2021-10-29 22:12:50 +0500

    Created Class-Based APIViews for delivery api

--- delivery/views.py
+++ delivery/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from .serializers import DeliverySerializer
+from .models import Order
+from rest_framework.views import  APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+
+
+class OrderListView(APIView):
+ 
+
+    def get(self, request):
+        buyurtma = Order.objects.all()
+        serializer = DeliverySerializer(buyurtma, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DeliverySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class OrderDetailView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        order = self.get_object(pk)
+        serializer = DeliverySerializer(order)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        order = self.get_object(pk)
+        serializer = DeliverySerializer(order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        order = self.get_object(pk)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
